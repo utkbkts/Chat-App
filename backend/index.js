8ca9e95ebae8e18857daa1877558d723eb5dff90
utku <utkbktss5@gmail.com>
2024-10-11 22:56:16 +0300

    update chat index.js

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,8 @@ import messageRouters from "./routes/message.routes.js";
 import ConnectedDatabase from "./db/mongoDB.js";
 import errorMiddleware from "./middleware/error.middleware.js";
 import { app, server } from "./socketIO/server.js";
+import path from "path";
 app.use(cookieParser());
-app.use(express.json());
 app.use(helmet());
 dotenv.config();
 app.use(
@@ -20,6 +20,9 @@ app.use(
     credentials: true,
   })
 );
+const __dirname = path.resolve();
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
   console.log(`Shutting Down server due to uncaught expection`);
@@ -36,6 +39,12 @@ server.listen(process.env.PORT, () => {
   ConnectedDatabase();
   console.log(`server is running PORT:${process.env.PORT}`);
 });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 process.on("unhandledRejection", (err) => {
   console.log(`Error ${err.message}`);
